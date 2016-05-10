071bdbedd9e26fd2a1428d0d9d9312eae618966f
EdgarOPG <eopg95@gmail.com>
2016-05-10 10:34:35 -0600

    Cambio en contactos

--- models/contactos.js
+++ models/contactos.js
@@ -3,9 +3,8 @@ var Schema = mongoose.Schema;
 var Usuario = mongoose.model('Usuario');
 
 var contactoSchema = new Schema({
-    nombre: String,
-    usuarioContacto: String/*,	
-    usuario: {type: Schema.ObjectId, ref: "Usuario"}*/
+    usuarioContacto: {type: Schema.ObjectId, ref: "Usuario"},	
+    usuario: {type: Schema.ObjectId, ref: "Usuario"}
 });
 //El mongose agarra el nombre en singular y lo transforma en plural y quita el capital para agregarlo a mongo
 var Contacto = mongoose.model("Contacto", contactoSchema);
